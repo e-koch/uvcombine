32f3dd35d8f7bd478d6ef864381e382badeb3799
Eric Koch <koch.eric.w@gmail.com>
2025-06-23 15:13:27 -0400

    Remove _astropy_init

--- uvcombine/__init__.py
+++ uvcombine/__init__.py
@@ -1,6 +1,6 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-from ._astropy_init import __version__, test
+from .version import version as __version__
 
 # For egg_info test builds to pass, put package imports here.
 from .uvcombine import (feather_plot, feather_simple, feather_compare,
